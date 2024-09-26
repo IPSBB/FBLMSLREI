4095703b4b089de2a36b2654e5446dc6e75450dc
IPSBB <justin.corbin@qbcaribbean.com>
2024-09-26 16:15:58 -0400

    Add Import module

---
+++ import_fees/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import harmonized_code
+from . import stock_landed_cost
+from . import account_move
+from . import purchase_order
+from . import product
+from . import res_config_settings
+from . import landed_cost_analysis_report
